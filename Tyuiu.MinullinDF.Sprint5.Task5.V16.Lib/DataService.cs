63ff111755b1e9c2b76beb51fe2ea0e5c6214c33
Dinar Minullin <Tyuiu.MinullinDF@outlook.com>
2025-11-24 16:41:27 +0500

    Task 5

---
+++ Tyuiu.MinullinDF.Sprint5.Task5.V16.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.MinullinDF.Sprint5.Task5.V16.Lib
+{
+    public class DataService : ISprint5Task5V16
+    {
+        public double LoadFromDataFile(string path)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            double a;
+            double c = 0, b = 0;
+            string[] nums = File.ReadAllText(path).Split(" ");
+
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                a = double.Parse(nums[i]);
+                if ((int)a == a && a % 10 == 0) 
+                {
+                    if (c == 0) 
+                    {
+                        b = a;
+                        c++;
+                    }
+                    else if (a > b) { b = a; }
+                }
+            }
+            return Math.Round(b, 3);
+        }
+    }
+}
---
+++ Tyuiu.MinullinDF.Sprint5.Task5.V16.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MinullinDF.Sprint5.Task5.V16.Lib;
+namespace Tyuiu.MinullinDF.Sprint5.Task5.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = $@"C:\DataSprint5\InPutDataFileTask5V16.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
