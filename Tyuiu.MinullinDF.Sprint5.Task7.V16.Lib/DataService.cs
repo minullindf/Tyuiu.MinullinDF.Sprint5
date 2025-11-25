d4e98a923734e249d2e7292f30b76187472dd20d
Dinar Minullin <Tyuiu.MinullinDF@outlook.com>
2025-11-25 23:51:27 +0500

    Task7.1

--- Tyuiu.MinullinDF.Sprint5.Task7.V16.Lib/DataService.cs
+++ Tyuiu.MinullinDF.Sprint5.Task7.V16.Lib/DataService.cs
@@ -6,30 +6,30 @@ namespace Tyuiu.MinullinDF.Sprint5.Task7.V16.Lib
     {
         public string LoadDataAndSave(string path)
         {
-            string pathSaveFile = Path.Combine(Path.GetTempPath(), "InPutDataFileTask7V16.txt");
-            //string pathSaveFile = $@"C:\DataSprint5\OutPutDataFileTask7V16.txt";
+            //string pathSaveFile = Path.Combine(Path.GetTempPath(), "InPutDataFileTask7V16.txt");
+            string pathSaveFile = $@"C:\DataSprint5\OutPutDataFileTask7V16.txt";
             FileInfo fileInfo = new FileInfo(pathSaveFile);
 
             if (fileInfo.Exists) { File.Delete(pathSaveFile); }
 
             string w = "";
             string[] words = [];
-            string[] newLines = [];
-            string[] newWords = [];
             string[] lines = File.ReadAllText(path).Split("\n");
+            string newLines = "";
             foreach (string line in lines)
             {
                 words = line.Split(" ");
+                string newWords = "";
                 foreach (string word in words)
                 {
                     w = word;
                     if (word.Length == 2) { w = "XY"; }
-                    newWords.Append(word);
+                    newWords += $"{w} ";
                 }
-                newLines.Append(String.Join(" ", newWords));
-                newWords = [];
+                newLines += $"{newWords.Trim()}\n";
+                newWords = "";
             }
-            string allText = String.Join("\n", newLines);
+            string allText = String.Join("\n", newLines.Trim());
             File.WriteAllText(pathSaveFile, allText);
             return pathSaveFile;
          }
