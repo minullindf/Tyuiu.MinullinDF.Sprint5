b8f8a6106c17558b57d5550f0d55db5a1be17eb0
Dinar Minullin <Tyuiu.MinullinDF@outlook.com>
2025-11-25 23:28:07 +0500

    Task7

---
+++ Tyuiu.MinullinDF.Sprint5.Task7.V16.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MinullinDF.Sprint5.Task7.V16.Lib;
+namespace Tyuiu.MinullinDF.Sprint5.Task5.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path1 = $@"C:\DataSprint5\InPutDataFileTask7V16.txt";
+            string path2 = $@"C:\DataSprint5\OutPutDataFileTask7V16.txt";
+            DataService ds = new DataService();
+            string newPath = ds.LoadDataAndSave(path1);
+            FileInfo fileInfo = new FileInfo(path2);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.MinullinDF.Sprint5.Task7.V16/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.MinullinDF.Sprint5.Task7.V16.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        string path = Path.Combine(Path.GetTempPath(), "InPutDataFileTask7V16.txt");
+        //string path = $@"C:\DataSprint5\InPutDataFileTask7V16.txt";
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+        Console.WriteLine("****************************************************************************");
+
+        Console.WriteLine($"путь = {path}");
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+        Console.WriteLine("****************************************************************************");
+
+        var res = ds.LoadDataAndSave(path);
+
+        Console.WriteLine("Ответ: " + res);
+        Console.ReadKey();
+    }
+}
